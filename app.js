05a1c7b3a800ef3c16b2313c157a5ba247825e4a
Sameerbaacha <sameeris6512@gmail.com>
2024-08-11 00:22:10 +0500

    update

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-
-function sortWord(word) {
-    return word.split('').sort().join('');
-}
-function findAnagrams(arr) {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        let currentWord = arr[i];
-        let currentSortedWord = sortWord(currentWord);
-        
-        let isAnagram = false;
-        
-        for (let j = 0; j < arr.length; j++) {
-            if (i !== j) {
-                let compareWord = arr[j];
-                let compareSortedWord = sortWord(compareWord);
-                
-                if (currentSortedWord === compareSortedWord) {
-                    isAnagram = true;
-                    break;
-                }
-            }
+function anagram(words) {
+    return Object.values(
+      words.reduce((acc, word) => {
+        const sortedWord = word.split('').sort().join('');
+        if (!acc[sortedWord]) {
+          acc[sortedWord] = [];
         }
-        
-        if (isAnagram) {
-            result.push(currentWord);
-        }
-    }
-
-    return result;
-}
-
-let arr = ["bat", "tap", "cat", "tab", "pat"];
-console.log(findAnagrams(arr));
+        acc[sortedWord].push(word);
+        return acc;
+    }, {})
+  );
+  }
+  console.log(anagram(['bat', 'tap', 'cat', 'tab', 'pat']));
+  
